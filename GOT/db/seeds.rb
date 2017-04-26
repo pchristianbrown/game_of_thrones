5a8300aef0264734bcea823d0de965856f7d6264
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-04-25 23:25:40 -0400

    Progress

--- GOT/db/seeds.rb
+++ GOT/db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-House.destroy.all
-Character.destroy.all
+House.destroy_all
+Character.destroy_all
 
 stark = House.create(name:"Stark", img_url:"https://vignette4.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest?cb=20170101103142")
 # tully = House.create(name:"Tully", img_url:"https://vignette3.wikia.nocookie.net/gameofthrones/images/b/bd/House-Tully-Main-Shield.PNG/revision/latest?cb=20160312103126")
 tyrell = House.create(name:"Tyrell", img_url:"https://vignette2.wikia.nocookie.net/gameofthrones/images/c/cf/House-Tyrell-Main-Shield.PNG/revision/latest?cb=20170108163035")
-# targaryen = House.create(name:"Targaryen", img_url:"https://vignette2.wikia.nocookie.net/gameofthrones/images/4/43/House-Targaryen-Main-Shield.PNG/revision/latest?cb=20161206005534")
+targaryen = House.create(name:"Targaryen", img_url:"https://vignette2.wikia.nocookie.net/gameofthrones/images/4/43/House-Targaryen-Main-Shield.PNG/revision/latest?cb=20161206005534")
 # # bolten = House.create(name:"Bolten", img_url:"https://vignette1.wikia.nocookie.net/gameofthrones/images/d/dd/House-Bolton-Main-Shield.PNG/revision/latest?cb=20161231131431")
 frey = House.create(name:"Frey", img_url:"https://vignette1.wikia.nocookie.net/gameofthrones/images/e/e6/House-Frey-Main-Shield.PNG/revision/latest?cb=20160312124244")
 greyjoy = House.create(name:"Greyjoy", img_url:"https://vignette3.wikia.nocookie.net/gameofthrones/images/8/86/House-Greyjoy-Main-Shield.PNG/revision/latest?cb=20160312122204")
@@ -42,10 +42,11 @@ eddard = Character.create(name:"Eddard Stark", img_url:"https://vignette1.wikia.
 robb = Character.create(name:"Robb Stark", img_url:"https://vignette2.wikia.nocookie.net/gameofthrones/images/5/50/S3E9_Robb_Stark_main.jpg/revision/latest?cb=20160718071203")
 arya = Character.create(name:"Arya Stark", img_url:"https://vignette2.wikia.nocookie.net/gameofthrones/images/e/ea/GoT-Sn7_FirstLook_10_.jpg/revision/latest?cb=20170420183850")
 sansa = Character.create(name:"Sansa Stark", img_url:"https://vignette4.wikia.nocookie.net/gameofthrones/images/6/68/Sansa_stark_s6_battle_bastards_infobox.jpg/revision/latest?cb=20160807084759")
+
 # House-Targaryen
 dany = Character.create(name:"Daenerys Targaryen", img_url:"https://vignette3.wikia.nocookie.net/gameofthrones/images/8/88/Daenerys_Targaryen_S7_Promo_Image.PNG/revision/latest?cb=20170420191154")
 viverys = Character.create(name:"Viserys Targaryen", img_url:"https://vignette2.wikia.nocookie.net/gameofthrones/images/4/46/Viseryspromoheadshot.jpg/revision/latest?cb=20160730184148")
 # House-Tarly
-
 # House-Tyrell
 olenna = Character.create(name:"Olenna Tyrell", img_url:"https://vignette1.wikia.nocookie.net/gameofthrones/images/b/bb/Olenna_season_6_a.jpg/revision/latest?cb=20160718204242")
+#
